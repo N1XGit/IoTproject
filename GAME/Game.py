c0f0160fb5789b8d7762e673ba96261c68ce7440
N1XGit <niilo.johannes.nk@gmail.com>
2025-02-07 13:30:27 +0200

    Update Game.py

--- GAME/Game.py
+++ GAME/Game.py
@@ -352,7 +352,7 @@ def main():
 
         data = {name:score}
         #if oldscore is None or score > oldscore:
-        requests.post(FIREBASE_URL, json=data)
+        requests.patch(FIREBASE_URL, json=data)
         
         lcd.write(" New game? (y/n)")
         newGame = input().lower()
